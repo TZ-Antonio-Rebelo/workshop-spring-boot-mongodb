94a507730c0ff53e209b99975f0c241053537408
TZ-Antonio-Rebelo <tz.ajsr@gmail.com>
2023-01-27 16:27:49 +0000

    Consulta com vários critérios

--- src/main/java/com/antoniorebelo/workshopmongo/services/PostService.java
+++ src/main/java/com/antoniorebelo/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.antoniorebelo.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,7 +31,13 @@ public class PostService {
 	
 	public List<Post> findByTitle(String text) { 
 		return repo.searchTitle(text); 
-		}
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000); // acrescentar 1dia ao maxDate - 24horas em milisegundos
+		return repo.fullSearch(text, minDate, maxDate);
+	}
 	
 
 }
